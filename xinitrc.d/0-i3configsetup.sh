7431ff1ad1917960be4dce905e23164d2fce3549
Frederik Wegner <onlyknoppas@gmail.com>
2019-05-29 16:40:11 +0200

    Type on i3 setup script.

--- xinitrc.d/0-i3configsetup.sh
+++ xinitrc.d/0-i3configsetup.sh
@@ -3,7 +3,7 @@
 # i3 config composition
 i3cfgdir=$HOME/.config/i3
 cat $i3cfgdir/base > $i3cfgdir/config
-if [ -d $i3cnfdir/local ] ; then
+if [ -d $i3cfgdir/local ] ; then
   for f in `ls $i3cfgdir/local` ; do
     cat $i3cfgdir/local/$f >> $i3cfgdir/config
   done
--- xinitrc.d/10-xrandr.sh
+++
@@ -1,1 +0,0 @@
-xrandr --output DP-1 --scale 1.25x1.25 --output DVI-D-0 --right-of DP-1
