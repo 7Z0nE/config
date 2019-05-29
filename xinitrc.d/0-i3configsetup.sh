c633a30371f28cf99ebaba6fdaa3eb79eee29b1c
Frederik Wegner <onlyknoppas@gmail.com>
2019-05-29 15:43:55 +0200

    Renamed scripts in xinitrc to .sh

---
+++ xinitrc.d/0-i3configsetup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# i3 config composition
+i3cfgdir=$HOME/.config/i3
+cat $i3cfgdir/base > $i3cfgdir/config
+if [ -d $i3cnfdir/local ] ; then
+  for f in `ls $i3cfgdir/local` ; do
+    cat $i3cfgdir/local/$f >> $i3cfgdir/config
+  done
+fi
---
+++ xinitrc.d/10-xrandr.sh
@@ -0,0 +1,1 @@
+xrandr --output DP-1 --scale 1.25x1.25 --output DVI-D-0 --right-of DP-1
